839cbcf8498ecffaa6b12616788a97c761fce105
surface03 <ginigini03@gmail.com>
2023-12-30 11:22:33 +0900

    add tab icon

--- app/src/main/java/com/example/kotlinfolio/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfolio/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         pager.adapter = pagerAdapter
         val tab = findViewById<TabLayout>(R.id.tabLayout)
         tab.setupWithViewPager(pager)
+        tab.getTabAt(1)?.setIcon(R.drawable.gallery_thumbnail)
     }
 
 }
